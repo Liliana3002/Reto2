c380a90b0bd95586adf3ef3682821be802af922c
Liliana <93170793+Liliana3002@users.noreply.github.com>
2022-12-30 21:21:21 -0500

    Reto2B

---
+++ src/PrimeraClase.java
@@ -0,0 +1,28 @@
+import java.util.*;//Texto que va fuera del código de ejecución
+public class PrimeraClase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hola Alba Liliana");
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println ("Por favor introduzca un número 1 por teclado: "); //Para ingresar información
+        int a = scan.nextInt();
+
+        System.out.println ("Por favor introduzca un número 2 por teclado: ");
+        int b = scan.nextInt();
+
+        System.out.println ("Por favor introduzca un número 3 por teclado: ");
+        int c = scan.nextInt();
+     
+scan.close();
+
+        System.out.println(a); //Para imprimir información
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+
+	}
+
+
